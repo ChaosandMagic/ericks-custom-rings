22394206a978cb433218b4295cc64128fcbd6a92
Chaosandmagic <chaosandmagicmex@gmail.com>
2025-08-12 17:02:56 -0300

    First Tokens
    Implementé los primeros tokens, Dead Suns y Extinction Curse, ambos funcionan

---
+++ scripts/ring-initialize.js
@@ -0,0 +1,31 @@
+
+
+Hooks.on("initializeDynamicTokenRingConfig", (ringConfig) => {
+
+  const dynamicRings = [
+    "dead-suns",
+    "extinction-curse"
+  ];
+
+  for (const ring of dynamicRings) {
+    const key = ring;
+    const label = ring.replace(/-/g, " ").replace(/\b\w/g, l => l.toUpperCase());
+    const spritesheet = `modules/ericks-custom-rings/Assets/token-rings/${ring}/${ring}.json`;
+    console.log("RING LOADED IS " + ring);
+
+    ringConfig.addConfig(key, new foundry.canvas.tokens.DynamicRingData({
+      key,
+      label,
+      spritesheet
+      ,
+      onload: (ring) => {
+        console.log(`Loaded ring ${key}`, ring);
+        console.log('Background frame:', ring.spritesheet.frames["ericks-token-rings"]);
+      }
+    })
+    );
+
+  };
+
+
+});
